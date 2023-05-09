6288fc1222096f66e222ea7d74d01268487aeaf7
Zachariah <zachruggiero@gmail.com>
2023-05-09 16:32:02 -0700

    dbSetup include

---
+++ dbSetup.sql
@@ -0,0 +1,141 @@
+--
+-- Script to drop prior tables 
+--
+
+SET FOREIGN_KEY_CHECKS = 0;
+SET @tarray = NULL;
+SELECT GROUP_CONCAT('`', table_name, '`') INTO @tarray
+  FROM information_schema.tables WHERE table_schema = (SELECT DATABASE());
+SELECT IFNULL(@tarray,'dummy') INTO @tarray;
+SET @tarray = CONCAT('DROP TABLE IF EXISTS ', @tarray);
+PREPARE stmt FROM @tarray;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
+SET FOREIGN_KEY_CHECKS = 1;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `userId` int(11) NOT NULL AUTO_INCREMENT,
+  `userName` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `active` tinyint(4) NOT NULL,
+  `createDate` datetime NOT NULL,
+  `createdBy` varchar(40) NOT NULL,
+  `lastUpdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `lastUpdateBy` varchar(40) NOT NULL,
+  PRIMARY KEY (`userId`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+
+--
+-- Table structure for table `country`
+--
+
+DROP TABLE IF EXISTS `country`;
+
+CREATE TABLE `country` (
+  `countryId` int(10) NOT NULL AUTO_INCREMENT,
+  `country` varchar(50) NOT NULL,
+  `createDate` datetime NOT NULL,
+  `createdBy` varchar(40) NOT NULL,
+  `lastUpdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `lastUpdateBy` varchar(40) NOT NULL,
+  PRIMARY KEY (`countryId`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+
+
+--
+-- Table structure for table `city`
+--
+
+DROP TABLE IF EXISTS `city`;
+
+CREATE TABLE `city` (
+  `cityId` int(10) NOT NULL AUTO_INCREMENT,
+  `city` varchar(50) NOT NULL,
+  `countryId` int(10) NOT NULL,
+  `createDate` datetime NOT NULL,
+  `createdBy` varchar(40) NOT NULL,
+  `lastUpdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `lastUpdateBy` varchar(40) NOT NULL,
+  PRIMARY KEY (`cityId`),
+  KEY `countryId` (`countryId`),
+  CONSTRAINT `city_ibfk_1` FOREIGN KEY (`countryId`) REFERENCES `country` (`countryId`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4;
+
+
+--
+-- Table structure for table `address`
+--
+
+DROP TABLE IF EXISTS `address`;
+
+CREATE TABLE `address` (
+  `addressId` int(10) NOT NULL AUTO_INCREMENT,
+  `address` varchar(50) NOT NULL,
+  `address2` varchar(50) NOT NULL,
+  `cityId` int(10) NOT NULL,
+  `postalCode` varchar(10) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `createDate` datetime NOT NULL,
+  `createdBy` varchar(40) NOT NULL,
+  `lastUpdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `lastUpdateBy` varchar(40) NOT NULL,
+  PRIMARY KEY (`addressId`),
+  KEY `cityId` (`cityId`),
+  CONSTRAINT `address_ibfk_1` FOREIGN KEY (`cityId`) REFERENCES `city` (`cityId`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Table structure for table `customer`
+--
+
+DROP TABLE IF EXISTS `customer`;
+
+CREATE TABLE `customer` (
+  `customerId` int(10) NOT NULL AUTO_INCREMENT,
+  `customerName` varchar(45) NOT NULL,
+  `addressId` int(10) NOT NULL,
+  `active` tinyint(1) NOT NULL,
+  `createDate` datetime NOT NULL,
+  `createdBy` varchar(40) NOT NULL,
+  `lastUpdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `lastUpdateBy` varchar(40) NOT NULL,
+  PRIMARY KEY (`customerId`),
+  KEY `addressId` (`addressId`),
+  CONSTRAINT `customer_ibfk_1` FOREIGN KEY (`addressId`) REFERENCES `address` (`addressId`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Table structure for table `appointment`
+--
+
+DROP TABLE IF EXISTS `appointment`;
+
+CREATE TABLE `appointment` (
+  `appointmentId` int(10) NOT NULL AUTO_INCREMENT,
+  `customerId` int(10) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `location` text NOT NULL,
+  `contact` text NOT NULL,
+  `type` text NOT NULL,
+  `url` varchar(255) NOT NULL,
+  `start` datetime NOT NULL,
+  `end` datetime NOT NULL,
+  `createDate` datetime NOT NULL,
+  `createdBy` varchar(40) NOT NULL,
+  `lastUpdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `lastUpdateBy` varchar(40) NOT NULL,
+  PRIMARY KEY (`appointmentId`),
+  KEY `userId` (`userId`),
+  KEY `appointment_ibfk_1` (`customerId`),
+  CONSTRAINT `appointment_ibfk_1` FOREIGN KEY (`customerId`) REFERENCES `customer` (`customerId`),
+  CONSTRAINT `appointment_ibfk_2` FOREIGN KEY (`userId`) REFERENCES `user` (`userId`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
